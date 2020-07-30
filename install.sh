775610d59a311b2914aedee24db30e373d92077a
bygreencn <bygreencn@gmail.com>
2020-07-30 18:16:57 +0800

    test

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+SH_PATH=$(cd "$(dirname "$0")";pwd)
+cd ${SH_PATH}
+
+create_mainfest_file(){
+    echo "进行配置。。。"
+    read -p "请输入你的应用名称：" IBM_APP_NAME
+    echo "应用名称：${IBM_APP_NAME}"
+    read -p "请输入你的应用内存大小(默认256)：" IBM_MEM_SIZE
+    if [ -z "${IBM_MEM_SIZE}" ];then
+	IBM_MEM_SIZE=256
+    fi
+    echo "内存大小：${IBM_MEM_SIZE}"
+    
+    
+    cat >  ${SH_PATH}/ibm-v2/v2cf/v2ray/manifest.yml  << EOF
+    applications:
+    - path: .
+      name: ${IBM_APP_NAME}
+      random-route: true
+      memory: ${IBM_MEM_SIZE}M
+EOF
+
+     echo "配置完成。"
+}
+
+clone_repo(){
+    echo "进行初始化。。。"
+    cd ${SH_PATH}
+    git clone https://github.com/bygreencn/ibm-v2
+    echo "初始化完成。"
+}
+
+install(){
+    echo "进行安装。。。"
+    cd ${SH_PATH}/ibm-v2/v2cf/v2ray
+    chmod +x *
+    cd ${SH_PATH}/ibm-v2/v2cf
+    ibmcloud target --cf
+    ibmcloud cf install
+    ibmcloud cf push
+    echo "安装完成。"
+}
+
+clone_repo
+create_mainfest_file
+install
+exit 0
---
+++ v2cf/main.py
@@ -0,0 +1,6 @@
+
+def main():
+    print("Hello IBM")
+
+if __name__ == "__main__":
+    main()
